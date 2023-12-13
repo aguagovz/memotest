fc722f6d5d1519aeba84c34b884af2b598252b04
aguagovz <aguagovz@gmail.com>
2023-12-13 01:12:49 -0300

    added data for memocards

---
+++ src/constants/furnitureList.js
@@ -0,0 +1,82 @@
+import schrank from "../assets/images/furniture/schrank.jpg";
+import stuhl from "../assets/images/furniture/stuhl.jpg";
+import tisch from "../assets/images/furniture/tisch.jpg";
+import lampe from "../assets/images/furniture/lampe.jpg";
+import bett from "../assets/images/furniture/bett.jpg";
+import regal from "../assets/images/furniture/regal.jpg";
+
+const memoCardChairName = {
+  title: "der Stuhl",
+  id: 1,
+};
+
+const memoCardChairImage = {
+  image: stuhl,
+  id: 1,
+};
+
+/*seguir haciendo esto*/
+
+const memoCardTableName = {
+  title: "der Tisch",
+  id: 2,
+};
+
+const memoCardTableImage = {
+  id: 2,
+  image: tisch,
+};
+
+const memoCardLampName = {
+  title: "die Lampe",
+  id: 3,
+};
+
+const memoCardLampImage = {
+  id: 3,
+  image: lampe};
+
+const memoCardBedName = {
+  title: "das Bett",
+  id: 4,
+};
+
+const memoCardBedImage = {
+  id: 4,
+  image: bett,
+};
+
+const memoCardCupboardName = {
+  title: "der Schrank",
+  id: 5,
+};
+
+const memoCardCupboardImage = {
+    id: 5,
+    image: schrank,
+  };
+
+const memoCardShelfName = {
+    title: "das Regal",
+    id: 6,
+  };
+
+  const memoCardShelfImage = {
+    id: 6,
+    image: regal,
+  };
+
+export const memoCardFurnitureList = [
+  memoCardBedImage,
+  memoCardChairImage,
+  memoCardCupboardImage,
+  memoCardTableName,
+  memoCardShelfImage,
+  memoCardLampImage,
+  memoCardBedName,
+  memoCardLampName,
+  memoCardChairName,
+  memoCardCupboardName,
+  memoCardShelfName,
+  memoCardTableImage,
+];
---
+++ src/constants/thingsList.js
@@ -0,0 +1,83 @@
+import flasche from "../assets/images/things/flasche.png";
+import kugelschreiber from "../assets/images/things/kugelschreiber.jpg";
+import brille from "../assets/images/things/brille.png";
+import tasche from "../assets/images/things/tasche.png";
+import kamera from "../assets/images/things/kamera.png";
+import handy from "../assets/images/things/handy.png";
+
+const memoCardPenName = {
+    title: "der Kugelschreiber",
+    id: 1,
+  };
+  
+  const memoCardPenImage = {
+    image: kugelschreiber,
+    id: 1,
+  };
+  
+  /*seguir haciendo esto*/
+  
+  const memoCardGlassesName = {
+    title: "die Brille",
+    id: 2,
+  };
+  
+  const memoCardGlassesImage = {
+    id: 2,
+    image: brille,
+  };
+  
+  const memoCardBottleName = {
+    title: "die Flasche",
+    id: 3,
+  };
+  
+  const memoCardBottleImage = {
+    id: 3,
+    image: flasche,
+};
+  
+  const memoCardBagName = {
+    title: "die Tasche",
+    id: 4,
+  };
+  
+  const memoCardBagImage = {
+    id: 4,
+    image: tasche,
+  };
+  
+  const memoCardCameraName = {
+    title: "die Kamera",
+    id: 5,
+  };
+  
+  const memoCardCameraImage = {
+      id: 5,
+      image: kamera,
+    };
+  
+  const memoCardCellphoneName = {
+      title: "das Handy",
+      id: 6,
+    };
+  
+    const memoCardCellphoneImage = {
+      id: 6,
+      image: handy,
+    };
+  
+  export const memoCardThingsList = [
+    memoCardPenImage,
+    memoCardGlassesImage,
+    memoCardBottleImage,
+    memoCardBagName,
+    memoCardCellphoneImage,
+    memoCardCameraImage,
+    memoCardCellphoneName,
+    memoCardGlassesName,
+    memoCardCameraName,
+    memoCardBottleName,
+    memoCardPenName,
+    memoCardBagImage,
+  ];
